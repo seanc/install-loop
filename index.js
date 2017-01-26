068d265fbbf47d12a837e9794ab81499987b2f20
Sean Wilson <hello@imsean.me>
2017-01-25 23:21:25 -0600

    now the loop

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const { exec } = require('child_process')
+const assign = require('json-assign')
+assign('./package.json', {
+  name: Math.random().toString(36).substr(2, 5)
+})
 exec('npm install seanc/install-loop', (err, out, stderr) => {
-  console.log(err)
+  console.log(out)
 })
