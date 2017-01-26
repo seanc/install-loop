fad14e533deb784d87b45644108ca3a873124ba0
Sean Wilson <hello@imsean.me>
2017-01-25 23:28:03 -0600

    ok mayb this time lol

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { exec } = require('child_process')
 const assign = require('json-assign')
-assign('./package.json', {
-  name: Math.random().toString(36).substr(2, 5)
-}, err => {
-  console.log(err)
-  exec('npm install seanc/install-loop', (err, out, stderr) => {
-    console.log(err, out, stderr)
+
+function again() {
+  assign('./package.json', {
+    name: Math.random().toString(36).substr(2, 5)
+  }, err => {
+    console.log(err)
+    exec('npm install seanc/install-loop', (err, out, stderr) => {
+      console.log(err, out, stderr)
+      again()
+    })
   })
-})
+}
